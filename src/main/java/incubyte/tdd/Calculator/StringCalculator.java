716a2171187fbbe19706f9c9bfdff46a921ee439
Romil Rabadiya <romilrabadiya@gmail.com>
2026-07-08 11:28:08 +0530

    🔴 test: add failing test for single number input

--- src/main/java/incubyte/tdd/Calculator/StringCalculator.java
+++ src/main/java/incubyte/tdd/Calculator/StringCalculator.java
@@ -3,6 +3,10 @@ package incubyte.tdd.Calculator;
 public class StringCalculator {
 
     public int add(String numbers) {
-        return 0;
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+
+        return Integer.parseInt(numbers);
     }
 }
