5bb0e23f8afbdd35c6603a636e7d62286e78c42d
Romil Rabadiya <romilrabadiya@gmail.com>
2026-07-08 11:42:51 +0530

    🟢 feat: support addition of multiple comma-separated numbers and 🔵 refactor: remove unnecessary single-number special case

--- src/main/java/incubyte/tdd/Calculator/StringCalculator.java
+++ src/main/java/incubyte/tdd/Calculator/StringCalculator.java
@@ -3,18 +3,18 @@ package incubyte.tdd.Calculator;
 public class StringCalculator {
 
     public int add(String numbers) {
-        // Minimal Code for Test4
         if (numbers.isEmpty()) {
             return 0;
         }
 
-        if (!numbers.contains(",")) {
-            return Integer.parseInt(numbers);
-        }
-
         String[] nums = numbers.split(",");
 
-        return Integer.parseInt(nums[0]) +
-                Integer.parseInt(nums[1]);
+        int sum = 0;
+
+        for (String num : nums) {
+            sum += Integer.parseInt(num);
+        }
+
+        return sum;
     }
 }
